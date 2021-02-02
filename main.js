c27cd4e7fe22ca6e23b9990e177ce9e1f327d287
Mohammad Zahidul Islam <34839948+imzooel@users.noreply.github.com>
2021-02-02 09:15:49 +0600

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ else {
 // output will be: condition is false
 
 
-let zakir = "0";
+
+
+let zooel = "0";
 if (zakir) {
   console.log(`condition is true`);
 }
@@ -16,3 +18,26 @@ else {
   console.log(`condition is false`);
 }
 //this will return true cause here 0 is considered as a string becasue of quotes
+
+
+
+
+let zakir = "";
+if (zakir) {
+  console.log(`condition is true`);
+}
+else {
+  console.log(`condition is false`);
+}
+//output will be false, because it's an empty string. same output will be given as 'SINGLE QUOTES' & `BACK TICKS`
+
+
+
+let zakir = {};
+if (zakir) {
+  console.log(`condition is true`);
+}
+else {
+  console.log(`condition is false`);
+}
+//return true for empty Object or empty Array
