cef1c0e07b737e07e1784ef2d225ca65a52f29c4
Mohammad Zahidul Islam <34839948+imzooel@users.noreply.github.com>
2021-02-02 10:50:51 +0600

    checking null vs undefined

---
+++ null vs undefined.js
@@ -0,0 +1,19 @@
+let imZooel;
+console.log(imZooel);
+console.log(typeof imZooel);
+// it will return undefined, cause the value for the imZooel variable hasn't been set yet.
+
+
+
+
+let imZooel = null;
+console.log(imZooel); //shows null
+console.log(typeof imZooel); //shows object
+// output will be null and object. cause null is nothing & it's data type is >>object
+
+
+
+console.log(null === undefined) // false
+console.log(null == undefined) // true
+console.log(null === null) // true
+//there is a twist between == double equal & === triple equal
