7f135c244df1095b16efc29f6128dd0bad3943b2
Mohammad Zahidul Islam <34839948+imzooel@users.noreply.github.com>
2021-02-02 09:11:29 +0600

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ else {
   console.log(`condition is false`);
 }
 // output will be: condition is false
+
+
+let zakir = "0";
+if (zakir) {
+  console.log(`condition is true`);
+}
+else {
+  console.log(`condition is false`);
+}
+//this will return true cause here 0 is considered as a string becasue of quotes
